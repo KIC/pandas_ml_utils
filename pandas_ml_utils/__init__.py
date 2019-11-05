3b78fbc0be3428438c5ef6cb16163d3bfd4017b6
kic <ch9.ki7@gmail.com>
2019-11-05 11:26:06 +0100

    fix links

--- pandas_ml_utils/__init__.py
+++ pandas_ml_utils/__init__.py
@@ -1,5 +1,5 @@
 """Augment pandas DataFrame with methods for machine learning"""
-__version__ = '0.0.12'
+__version__ = '0.0.13'
 
 from .pandas_utils_extension import *
 from .analysis.correlation_analysis import *
