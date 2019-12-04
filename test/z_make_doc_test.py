da0d006a3a98ea7b1c4b1b7aaf89b4acde95fa21
kic <ch9.ki7@gmail.com>
2019-12-04 16:45:25 +0100

    exculde doc test in github

--- test/z_make_doc_test.py
+++ test/z_make_doc_test.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 import subprocess
 from unittest import TestCase
 
@@ -7,6 +8,7 @@ path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "docs")
 
 class TestMakeDocs(TestCase):
 
+    @pytest.mark.skipif(os.environ.get('USER') != 'kic', reason="do not run this test in github actions")
     def test_make_clean_html(self):
         """when"""
         docs_process = subprocess.run(["make", "clean", "html"], cwd=path, capture_output=True)
